839a3a1f8e9e0a5034dd8129d91d5153a93bd3b0
christiancmota <christian.kfsn@gmail.com>
2024-06-12 03:53:28 +0100

    keydown to play the drums

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -39,3 +39,34 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     });
 }
 
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.addEventListener("keydown", function (event) {
+        switch (event.key) {
+            case "w":
+                drumAudios[0].play();
+                break;
+            case "a":
+                drumAudios[1].play();
+                break;
+            case "s":
+                drumAudios[2].play();
+                break;
+            case "d":
+                drumAudios[3].play();
+                break;
+            case "j":
+                drumAudios[4].play();
+                break;
+            case "k":
+                drumAudios[5].play();
+                break;
+            case "l":
+                drumAudios[6].play();
+                break;
+            default: drumAudios[0].play();
+                break;
+        }
+    });
+}
+
